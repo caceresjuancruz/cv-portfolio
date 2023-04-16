4929c28cc3fa67eb778fb0db7dec8e949ac99d09
Jose Leon Caceres Musso <caceresjuan.dev@gmail.com>
2023-04-16 00:07:32 -0300

    pdf download update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,8 @@ function Header() {
   return (
     <div className="row p-0 m-0">
       <div className="col-12 text-end p-0">
+        {" "}
+        {/* style={{ visibility: "hidden" }} */}
         <NavLink to="/">
           <button className="btn btn-sm btn-light text-secondary me-2 mt-2">
             ESP
