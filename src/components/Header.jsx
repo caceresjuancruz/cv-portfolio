121401f163e8cb42801d5a1724a43165570724f8
Juan Cruz Cáceres <106564947+caceresjuancruz@users.noreply.github.com>
2024-02-06 08:56:48 -0300

    Remove hidden visibility style from Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 function Header() {
   return (
     <div className="row p-0 m-0">
-      <div className="col-12 text-end p-0" style={{ visibility: "hidden" }}>
+      <div className="col-12 text-end p-0">
         {/* style={{ visibility: "hidden" }} */}
         <NavLink to="/">
           <button className="btn btn-sm btn-light text-secondary me-2 mt-2">
